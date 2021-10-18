069f092a7fcb0cf3b4bbb881aa594ea3905d7530
bobbintb <bobbintb@gmail.com>
2021-10-18 10:57:23 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-install_requires=[
-   'sqlalchemy',
-   'sqlalchemy_utils'
-]
+
 
 setup(
     name='traversey',
     version='1.0',
     packages=[''],
+    install_requires=[
+        'sqlalchemy',
+        'sqlalchemy_utils'
+    ]
     url='',
     license='',
     author='',
